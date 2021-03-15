50af032d601df6e805b7f889c543b310f0d3d8ec
jab108 <jab108@bucknell.edu>
2021-03-15 01:02:54 -0400

    Created initial pizza ordering screen

--- app/src/main/java/com/example/jonnyspizza/MainActivity.java
+++ app/src/main/java/com/example/jonnyspizza/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.jonnyspizza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void orderPizza(View view){
+        View v = findViewById(R.id.orderPizzaBtn);
+        Intent intent = new Intent(this, OrderPizza.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/jonnyspizza/OrderPizza.java
@@ -0,0 +1,14 @@
+package com.example.jonnyspizza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderPizza extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_pizza);
+    }
+}
